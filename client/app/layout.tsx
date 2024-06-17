dcdf736f296b57a6ce726a0d6e1cb00bf9725312
lazybug19 <behera.santrupti02@gmail.com>
2024-06-17 13:36:24 +0530

    layout changes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow bg-gradient-to-r from-sky-500 to-indigo-500">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,14 +11,13 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <h1 className={title()}>Fake News Detector</h1>
         <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
+        <h3 className={title()}>
+          Your shield against misinformation.
+        </h3>
         <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+          Get credibility scores of news articles in real time.
         </h2>
       </div>
 
@@ -26,21 +25,13 @@ export default function Home() {
         <Link
           isExternal
           className={buttonStyles({
-            color: "primary",
+            color: "default",
             radius: "full",
             variant: "shadow",
           })}
           href={siteConfig.links.docs}
         >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
+          Check
         </Link>
       </div>
 
